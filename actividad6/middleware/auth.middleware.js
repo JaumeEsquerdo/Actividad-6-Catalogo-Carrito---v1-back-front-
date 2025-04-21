c6ea3f84753da03e334079216a2fab6ed3bc6f8a
Jaume Esquerdo <jaume.esquerdo@alumnos.cei.es>
2025-04-21 19:46:42 +0200

    Update auth.middleware.js

--- actividad6/middleware/auth.middleware.js
+++ actividad6/middleware/auth.middleware.js
@@ -14,12 +14,12 @@ export const authMiddleWare = (req, res, next) => {
         // verificar el token
         const decoded = jwt.verify(token, JWT_SECRET);
         req.userId = decoded.userId // guardo el id extraido del token en el request
-        next();
+        next(); //sigue hacia el siguiente controlador
         // req.userId se obtiene del token descifrado y solo existe durante la petición. No se almacena en el token ni en la base de datos.
 
 
-    } catch {
-
+    } catch(err) {
+        console.error("error en el token", err)
         res.status(401).json({ message: "Acceso denegado. Token inválido o expirado" })
     }
 }
