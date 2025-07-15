a773130665b7b9d69a4125d12125f9f7379c2ccb
Jaume Esquerdo <jaume.esquerdo@alumnos.cei.es>
2025-07-15 17:23:10 +0200

    juntar en la logica de abrir modal producto al nombre

--- front-actividad6/front-actividad6/src/pages/Menu.jsx
+++ front-actividad6/front-actividad6/src/pages/Menu.jsx
@@ -318,30 +318,33 @@ export const GaleriaMenu = ({ getStrokeColor, products, addToCart, backendURL, s
             items.push(
                 <div key={product._id} className="Card">
                     <div className="Card-imageWrapper" onClick={() => setProductoSeleccionado(product)}>
-                        <img
-                            className="Card-img"
-                            src={imageUrl || '/img/imagen-no-encontrada.jpg'}
-                            alt={product.name}
-                        />
-                        <svg
-                            className="Card-imageStroke"
-                            width="140"
-                            height="140"
-                            viewBox="0 0 140 140"
-                            fill="none"
-                            xmlns="http://www.w3.org/2000/svg"
-                        >
-                            <path
-                                d="M70 5 C95 5 130 20 130 70 C130 115 95 135 70 135 C45 135 10 115 10 70 C10 25 45 5 70 5 Z"
-                                stroke={strokeColor}
-                                strokeWidth="8"
-                                fill="none"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
+                        <div className='Card-media'>
+                            <img
+                                className="Card-img"
+                                src={imageUrl || '/img/imagen-no-encontrada.jpg'}
+                                alt={product.name}
                             />
-                        </svg>
+                            <svg
+                                className="Card-imageStroke"
+                                width="140"
+                                height="140"
+                                viewBox="0 0 140 140"
+                                fill="none"
+                                xmlns="http://www.w3.org/2000/svg"
+                            >
+                                <path
+                                    d="M70 5 C95 5 130 20 130 70 C130 115 95 135 70 135 C45 135 10 115 10 70 C10 25 45 5 70 5 Z"
+                                    stroke={strokeColor}
+                                    strokeWidth="8"
+                                    fill="none"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                />
+                            </svg>
+
+                        </div>
+                        <p className="Card-name">{product.name}</p>
                     </div>
-                    <p className="Card-name">{product.name}</p>
                     <p className="Card-price">{product.precio}€</p>
                     <button className='Card-btn' onClick={() => addToCart(product)}>Añadir</button>
                 </div>
