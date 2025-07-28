75d5f254093bf0bec2f382600a4a900d47898aa4
Jaume Esquerdo <jaume.esquerdo@alumnos.cei.es>
2025-07-28 21:12:57 +0200

    Update MesaHeader.jsx

--- front-actividad6/front-actividad6/src/components/MesaHeader.jsx
+++ front-actividad6/front-actividad6/src/components/MesaHeader.jsx
@@ -1,7 +1,12 @@
-export const MesaHeader = ({mesa}) => {
+export const MesaHeader = ({ mesa }) => {
     return (<>
 
-        <h1 className="Menu-h1">SUSHIRO</h1>
+        <header className="Menu-header">
+            <h1 className="Menu-h1">SUSHIRO</h1>
+            <div className="Header-icons">
+                iconos de salir / cambiar mesa y de ver pedidos realizados
+            </div>
+        </header>
         <h2 className='Menu-h2'>
             {mesa && mesa.numero
                 ? `Mesa ${mesa.numero} haciendo su pedido`
